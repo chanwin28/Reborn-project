23957dc2e74be1b7460184614897532230277c2f
chanwin28 <soeaungkyaw0021@gmail.com>
2025-11-13 21:40:33 +0630

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkTRy0nhb_mUOmoaGQZwouMJPCC37uRRQ",
-  authDomain: "plexiform-being-471915-u1.firebaseapp.com",
-  projectId: "plexiform-being-471915-u1",
-  storageBucket: "plexiform-being-471915-u1.firebasestorage.app",
-  messagingSenderId: "477134304630",
-  appId: "1:477134304630:web:a7af00f276bdcfd2c77657",
-  measurementId: "G-VDCF2TGMET"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MSG_ID",
+  appId: "YOUR_APP_ID"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
